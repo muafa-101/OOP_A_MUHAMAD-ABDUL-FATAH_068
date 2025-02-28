d95d8ab41af4ada4ec3bd6abe5edd45f3974edd5
unknown <makandonat101@webmail.umm.ac.id>
2025-02-28 09:44:26 +0800

    Second commit

--- Codelab/Module_1/src/Main.java
+++ Codelab/Module_1/src/Main.java
@@ -1,24 +1,29 @@
-import java.util.Scanner;
+import java.util.Scanner;//API for scan
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //scan your name
         System.out.print("Enter Your Name: ");
         String name = sc.nextLine();
 
+
+        //condition for gender
         System.out.print("Enter Your Gender (M/F): ");
         String gender = sc.nextLine();
-        if (gender.equalsIgnoreCase("M")) {
+        if (gender.equalsIgnoreCase("M")) {//ignoring upper and lower case
             gender = "Male";
-        }else if (gender.equalsIgnoreCase("F")) {
+        }else if (gender.equalsIgnoreCase("F")) {//ignoring upper and lower case
             gender = "Female";
-        }else System.out.println("Invalid Gender");
+        }else System.out.println("Invalid Gender");//if the gender not female or male, invalid input
 
+        //scan birth year
         System.out.print("Enter Your Birth Year: ");
         int birthYear = sc.nextInt();
         System.out.println();
 
+        //after scan all data, print all data
         System.out.println("Personal Data: ");
         System.out.println("Name: " + name);
         System.out.println("Gender: " + gender);

14c459ab4f7f9a40ca9cbf4c769e4f5cd764f176
unknown <makandonat101@webmail.umm.ac.id>
2025-03-18 18:43:33 +0800

    update module 1

--- Task_OOP/src/LoginSystem_Module_1.java
+++ Task_OOP/src/LoginSystem_Module_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LoginSystem  {
+public class LoginSystem_Module_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean loggedin = false;//to save the return value

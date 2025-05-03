eab1d8b2a63f3407d72991bc2a698c16e4da4d3b
muafa-101 <kemet826@gmail.com>
2025-05-03 22:44:19 +0800

    update task 3

--- Task_OOP/src/AdminClass_Module_3.java
+++ Task_OOP/src/AdminClass_Module_3.java
@@ -5,12 +5,8 @@ public class AdminClass_Module_3 extends UserClass_Module_3 {
 
     @Override
     public void login(UserClass_Module_3 user){
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> e0903ca (first commit module 3)
+        if this.name.equals(user.name)
     }
 
-    }
 }
+
